30f20ca6d67af6ae35774cec7df881350524ee64
irfan_usuf <irfanusuf33@gmail.com>
2024-10-12 11:15:34 +0530

    v1.1.0

---
+++ models/userModel.js
@@ -0,0 +1,16 @@
+
+
+const mongoose = require("mongoose")
+
+const User = mongoose.model("User" , {
+
+username : {type : String , require : true},
+email : {type : String , require : true},
+password : {type : String , require : true},
+profilePicUrl: {type : String },
+
+
+})
+
+
+module.export = {User}
